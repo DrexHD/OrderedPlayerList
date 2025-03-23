fbb1b1b96b4285f37bdccc4a96e4355e65bce8a6
Drex <nicknamedrex@gmail.com>
2025-03-23 11:18:17 +0100

    Update to 1.21.5-rc1

--- src/main/java/me/drex/orderedplayerlist/config/ConfigManager.java
+++ src/main/java/me/drex/orderedplayerlist/config/ConfigManager.java
@@ -1,7 +1,8 @@
 package me.drex.orderedplayerlist.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
 import me.drex.orderedplayerlist.OrderedPlayerList;
 import me.drex.orderedplayerlist.config.sequence.Sequence;
 import me.drex.orderedplayerlist.config.sequence.util.ComparisonMode;
@@ -12,17 +13,21 @@ import me.drex.orderedplayerlist.util.serializer.SequenceSerializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resources.ResourceLocation;
 
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ConfigManager {
 
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().setLenient()
-            .registerTypeAdapter(PlayerComparator.class, new PlayerComparatorSerializer())
-            .registerTypeAdapter(ComparisonMode.class, new ComparisonModeSerializer())
-            .registerTypeAdapter(Sequence.class, SequenceSerializer.INSTANCE)
-            .registerTypeHierarchyAdapter(ResourceLocation.class, new ResourceLocation.Serializer())
-            .create();
+    private static final Gson GSON = new GsonBuilder()
+        .setPrettyPrinting()
+        .disableHtmlEscaping()
+        .setStrictness(Strictness.LENIENT)
+        .registerTypeAdapter(PlayerComparator.class, new PlayerComparatorSerializer())
+        .registerTypeAdapter(ComparisonMode.class, new ComparisonModeSerializer())
+        .registerTypeAdapter(Sequence.class, SequenceSerializer.INSTANCE)
+        .registerTypeHierarchyAdapter(ResourceLocation.class, new RegistryUnawareCodecSerializer<>(ResourceLocation.CODEC))
+        .create();
     private static final Path CONFIG_FILE = FabricLoader.getInstance().getConfigDir().resolve("ordered-playerlist.json");
 
     private ConfigManager() {
@@ -44,6 +49,26 @@ public class ConfigManager {
             } catch (Exception ex) {
                 OrderedPlayerList.LOGGER.error("Failed to save config file!", ex);
                 return false;
+            }
+        }
+    }
+
+    private record RegistryUnawareCodecSerializer<T>(Codec<T> codec) implements JsonSerializer<T>, JsonDeserializer<T> {
+        @Override
+        public T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            try {
+                return this.codec.decode(JsonOps.INSTANCE, json).getOrThrow().getFirst();
+            } catch (Throwable e) {
+                return null;
+            }
+        }
+
+        @Override
+        public JsonElement serialize(T src, Type typeOfSrc, JsonSerializationContext context) {
+            try {
+                return src != null ? this.codec.encodeStart(JsonOps.INSTANCE, src).getOrThrow() : JsonNull.INSTANCE;
+            } catch (Throwable e) {
+                return JsonNull.INSTANCE;
             }
         }
     }
